07d2b5634765f15d46c9a16869661ed5d9857c24
aginert <ana.ginert@gmail.com>
2019-02-26 10:46:20 +0100

    Estructura de oproyecto creada. Css creado. Y el componente del formulario

---
+++ src/componentes/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Header from './header';
+
+class App extends Component {
+  render() {
+    return (
+        <div className="contenedor">
+          <Header
+          titulo='Cotizador de Seguro de coche'/>
+          <div className="contenedor-formulario">
+
+          </div>
+        </div>
+
+    );
+  }
+}
+
+export default App;
---
+++ src/componentes/header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = props => {
+    return (
+        <header className="top">
+            {props.titulo}
+        </header>
+    )
+};
+export default Header;
